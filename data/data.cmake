f374c49418e254d48b0aa702f07ffec011720b36
dingjing <dingjing@live.cn>
2025-05-06 09:18:24 +0800

    [DEL] qt、glib

--- data/data.cmake
+++ data/data.cmake
@@ -1,6 +1,6 @@
 install(FILES
         ${CMAKE_SOURCE_DIR}/data/clibrary-c.pc
-        ${CMAKE_SOURCE_DIR}/data/clibrary-qt5.pc
-        ${CMAKE_SOURCE_DIR}/data/clibrary-glib.pc
         ${CMAKE_SOURCE_DIR}/data/clibrary-c-static.pc
 		DESTINATION lib/pkgconfig/)
+#${CMAKE_SOURCE_DIR}/data/clibrary-qt5.pc
+#${CMAKE_SOURCE_DIR}/data/clibrary-glib.pc
