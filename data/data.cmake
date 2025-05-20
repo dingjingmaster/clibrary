c6a134a061dc2db7f42afc7da3fab9bce4da8a04
dingjing <dingjing@live.cn>
2025-05-20 17:20:15 +0800

    [ADD] lib64

--- data/data.cmake
+++ data/data.cmake
@@ -1,6 +1,6 @@
 install(FILES
         ${CMAKE_SOURCE_DIR}/data/clibrary-c.pc
         ${CMAKE_SOURCE_DIR}/data/clibrary-c-static.pc
-		DESTINATION lib/pkgconfig/)
+		DESTINATION lib64/pkgconfig/)
 #${CMAKE_SOURCE_DIR}/data/clibrary-qt5.pc
 #${CMAKE_SOURCE_DIR}/data/clibrary-glib.pc
