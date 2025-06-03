bfdcbcefcbbefbedc9e4e9450904d6cd3de53f48
dingjing <dingjing@live.cn>
2025-06-03 10:07:10 +0800

    [FIX] 生成 .pc 文件

--- data/data.cmake
+++ data/data.cmake
@@ -1,6 +1,18 @@
+configure_file(
+        ${CMAKE_SOURCE_DIR}/data/clibrary-c-static.pc.in
+        ${CMAKE_BINARY_DIR}/data/clibrary-c-static.pc
+        @ONLY
+)
+
+configure_file(
+        ${CMAKE_SOURCE_DIR}/data/clibrary-c.pc.in
+        ${CMAKE_BINARY_DIR}/data/clibrary-c.pc
+        @ONLY
+)
+
 install(FILES
-        ${CMAKE_SOURCE_DIR}/data/clibrary-c.pc
-        ${CMAKE_SOURCE_DIR}/data/clibrary-c-static.pc
-		DESTINATION lib64/pkgconfig/)
+        ${CMAKE_BINARY_DIR}/data/clibrary-c.pc
+        ${CMAKE_BINARY_DIR}/data/clibrary-c-static.pc
+        DESTINATION lib64/pkgconfig/)
 #${CMAKE_SOURCE_DIR}/data/clibrary-qt5.pc
 #${CMAKE_SOURCE_DIR}/data/clibrary-glib.pc
